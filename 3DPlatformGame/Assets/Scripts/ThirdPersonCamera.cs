9a2d9ba458e7411be39a5e4c131a7b731e73d7bc
Jesse Ruhl <60628685+jesseruhlz@users.noreply.github.com>
2022-07-11 13:31:01 -0700

    Player Movement
    Added player movement that moves player relative to camera direction.

---
+++ 3DPlatformGame/Assets/Scripts/ThirdPersonCamera.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    [Header("References")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObject;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        // turn off cursor
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        // rotate orientation
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        // rotate player object
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector3 inputDir = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        if (inputDir != Vector3.zero)
+            playerObject.forward = Vector3.Slerp(playerObject.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+    }
+}
